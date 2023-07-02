384699de558d4f1b638ce9e5acd2be8d829b6bc5
michaeljb73 <michaeljb73@gmail.com>
2023-07-01 23:47:13 -0300

    Terceiro commit

--- public/index.php
+++ public/index.php
@@ -5,10 +5,28 @@
 // var_dump($cons);
 
 $myName = 'Michael';
+$num = 22;
+
+
+define("FOO", "alguma coisa");
+
+const alfa = 10;
+
+if($num >= 18){
+    echo "Verdade\n";
+}
+
+$lima = FOO;
 
 echo $myName;
 
+echo alfa;
+
 // string
 // echo gettype($myName)
+
+/**
+ *  Teste de comentário
+ */
 
 ?>
